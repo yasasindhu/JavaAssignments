453eec39a9ec5102b90c4261178215e68ec276c8
“sindhu <yasasindhu32@gmail.com>
2021-05-12 18:37:26 +0530

    assignment-5 with task to create package with my name

---
+++ sindhu/assignment/Data.java
@@ -0,0 +1,17 @@
+package sindhu.assignment;
+
+public class Data {
+    int num;
+    char element;
+    public void printValue(){
+        System.out.println(num);
+        System.out.println(element);
+    }
+    public void  printLocalVariable(){
+      int localNum ;
+        char localElement;
+      //system.out.println(localNum);
+    // System.out.println(localElement);
+}
+}
+//local variables cannot be accessed without initialization
